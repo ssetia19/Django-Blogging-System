902c7477fc360eb8be43939cca94b588ae90dbb3
ssetia19 <ssetia.nitk@gmail.com>
2018-12-28 15:35:55 +0530

    my first commit

---
+++ articles/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+
+#namespacing
+app_name = 'articles'
+
+urlpatterns = [
+    #r means raw string
+ 	url(r'^$',views.article_list, name ="list"),
+ 	url(r'^create/$',views.article_create,name="create"),
+ 	#below is a name capturing group ?P<name of thing we want to capture>   
+ 	url(r'^(?P<slug>[\w-]+)/$',views.article_detail,name="detail"),
+ ]
